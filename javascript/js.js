804a61dd37d9a9a753016ce470e7ffe42a110d1c
Derek Shue <dshue@berkeley.edu>
2020-03-08 21:53:45 -0700

    Add ismatch

--- javascript/js.js
+++ javascript/js.js
@@ -184,5 +184,160 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
 };
 
-console.log(findMedianSortedArrays([1,3],[2]));
-console.log(findMedianSortedArrays([1,2],[3,4]));
+// console.log(findMedianSortedArrays([1,3],[2]));
+// console.log(findMedianSortedArrays([1,2],[3,4]));
+
+// Implement atoi which converts a string to an integer.
+
+// The function first discards as many whitespace characters as necessary until the first
+// non-whitespace character is found. Then, starting from this character, takes an optional 
+// initial plus or minus sign followed by as many numerical digits as possible, and interprets 
+// them as a numerical value.
+
+// The string can contain additional characters after those that form the integral number, 
+// which are ignored and have no effect on the behavior of this function.
+
+// If the first sequence of non-whitespace characters in str is not a valid integral number, 
+// or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+// If no valid conversion could be performed, a zero value is returned.
+
+// Note:
+
+// Only the space character ' ' is considered as whitespace character.
+// Assume we are dealing with an environment which could only store integers within the 
+// 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range 
+// of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+
+// Example 1:
+
+// Input: "42"
+// Output: 42
+// Example 2:
+
+// Input: "   -42"
+// Output: -42
+// Explanation: The first non-whitespace character is '-', which is the minus sign.
+//              Then take as many numerical digits as possible, which gets 42.
+// Example 3:
+
+// Input: "4193 with words"
+// Output: 4193
+// Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+// Example 4:
+
+// Input: "words and 987"
+// Output: 0
+// Explanation: The first non-whitespace character is 'w', which is not a numerical 
+//              digit or a +/- sign. Therefore no valid conversion could be performed.
+// Example 5:
+
+// Input: "-91283472332"
+// Output: -2147483648
+// Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+//              Thefore INT_MIN (−231) is returned.
+
+var myAtoi = function(str) {
+    intStr = "";
+    for (let i=0; i<str.length; i++){
+        if (str[i] === " " || (str[i] === "0" && (!intStr.length || intStr === "+" || intStr === "-"))){
+            continue;
+        } else if (!parseInt(str[i]) && str[i] !== "+" && str[i] !== "-" & str[i] !== "0") {
+            break;
+        } else {
+            if ((str[i] === "+" || str[i] === "-") && intStr.length) break;
+            intStr += str[i];
+        }
+    }
+    let sign;
+    if (intStr[0] === "+"|| intStr[0] === "-"){
+        if (intStr.length === 1 || !parseInt(intStr[1])) return 0;
+        sign = intStr[0];
+        let int = parseInt(intStr.slice(1));
+        if (int > 2**31) return sign === "+" ? 2**31 : 2**31 * -1;
+        return sign === "-" ? int * -1 : int;
+    } else {
+        if (!intStr.length) return 0;
+        return parseInt(intStr) > 2**31 ? 2 ** 31 : parseInt(intStr);
+    }
+};
+
+// Given an input string (s) and a pattern (p), implement regular expression matching with 
+// support for '.' and '*'.
+
+// '.' Matches any single character.
+// '*' Matches zero or more of the preceding element.
+// The matching should cover the entire input string (not partial).
+
+// Note:
+
+// s could be empty and contains only lowercase letters a-z.
+// p could be empty and contains only lowercase letters a-z, and characters like . or *.
+// Example 1:
+
+// Input:
+// s = "aa"
+// p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+
+// Input:
+// s = "aa"
+// p = "a*"
+// Output: true
+// Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+// Example 3:
+
+// Input:
+// s = "ab"
+// p = ".*"
+// Output: true
+// Explanation: ".*" means "zero or more (*) of any character (.)".
+// Example 4:
+
+// Input:
+// s = "aab"
+// p = "c*a*b"
+// Output: true
+// Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+// Example 5:
+
+// Input:
+// s = "mississippi"
+// p = "mis*is*p*."
+// Output: false
+
+var isMatch = function(str, p) {
+    let strIdx = 0, pIdx = 0;
+    while (strIdx != str.length && pIdx != p.length){
+        if (p[pIdx + 1] === "*"){
+            if (p[pIdx] === ".") {
+                if (pIdx === p.length - 2){
+                    return true;
+                } else {
+                    let next = p[pIdx + 2];
+                    if (str[strIdx] === next){
+                        pIdx += 3;
+                    }
+                    strIdx++;
+                }
+            } else if (str[strIdx] === p[pIdx]){
+                strIdx++;
+                console.log(strIdx, pIdx);
+                if (pIdx === p.length - 2 && strIdx === str.length) return true;
+            } else {
+                pIdx += 2;
+            }
+        } else if (p[pIdx] === "." || p[pIdx] === str[strIdx]){
+            strIdx++;
+            pIdx++;
+        } else {
+            return false;
+        }
+    };
+    if (strIdx != str.length || pIdx != p.length) return false;
+    return true;
+};
+
+console.log(isMatch("aa", "a*"));
