c5ace193a1c2f483179931d8fba2190756d6123e
Derek Shue <dshue@berkeley.edu>
2020-03-17 19:55:22 -0700

    Add remove duplicates

---
+++ ruby/remove_duplicates.rb
@@ -0,0 +1,41 @@
+# Given a sorted array nums, remove the duplicates in-place such that each element appear 
+# only once and return the new length.
+
+# Do not allocate extra space for another array, you must do this by modifying the input 
+# array in-place with O(1) extra memory.
+
+# Example 1:
+
+# Given nums = [1,1,2],
+
+# Your function should return length = 2, with the first two elements of nums being 
+# 1 and 2 respectively.
+
+# It doesn't matter what you leave beyond the returned length.
+# Example 2:
+
+# Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+# Your function should return length = 5, with the first five elements of nums being 
+# modified to 0, 1, 2, 3, and 4 respectively.
+
+# It doesn't matter what values are set beyond the returned length.
+
+require 'Set'
+
+def remove_duplicates(nums)
+    set = Set.new
+    idx = 0
+    while nums[idx] do
+       if set.include?(nums[idx])
+           nums.delete_at(nums[idx])
+       else
+           set.add(nums[idx]) 
+           idx += 1
+       end
+    end
+    nums
+end
+
+p remove_duplicates([1,1,2])
+p remove_duplicates([0,0,1,1,1,2,2,3,3,4])
