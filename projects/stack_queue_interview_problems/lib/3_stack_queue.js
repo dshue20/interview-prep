97b598cc2f8f02195c98a5af375a525a4f90835f
Derek Shue <dshue@berkeley.edu>
2020-04-20 18:25:02 -0700

    Add stack queue

--- projects/stack_queue_interview_problems/lib/3_stack_queue.js
+++ projects/stack_queue_interview_problems/lib/3_stack_queue.js
@@ -26,17 +26,77 @@
 
 class Node {
     // TODO: Implement the Node class!
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
     // TODO: Implement the Stack class!
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(node){
+        const newNode = new Node(node.value);
+        newNode.next = this.top;
+        this.top = newNode;
+        this.length++;
+        if (!this.bottom) this.bottom = newNode;
+        return this.length;
+    }
+
+    pop(){
+        if (!this.length) return null;
+        const temp = this.top;
+        this.top = this.top.next;
+        this.length--;
+        if (!this.length) this.bottom = null;
+        return temp;
+    }
+
+    size(){
+        return this.length;
+    }
 }
 
 class StackQueue {
-    // TODO: Implement the StackQueue class!
+    constructor(){
+        this.inStack = new Stack();
+        this.outStack = new Stack();
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    enqueue(val){
+        const node = new Node(val);
+        if (!this.front) this.front = node;
+        if (this.back) this.back.next = node;
+        this.back = node;
+        this.inStack.push(node);
+        this.length++;
+        return this.length;
+    }
+
+    dequeue(){
+        if (!this.length) return null;
+        this.front = this.front.next;
+        while (this.inStack.length){
+            this.outStack.push(this.inStack.pop());
+        };
+        this.length--;
+        console.log(this.length);
+        if (!this.length) this.back = null;
+        return this.outStack.pop();
+    }
+
+    size(){
+        return this.length;
+    }
 };
 
 exports.Node = Node;
