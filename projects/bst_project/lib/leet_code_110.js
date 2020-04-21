359ad0a93722148199ec142f3d5a7501430c9bfd
Derek Shue <dshue@berkeley.edu>
2020-04-21 14:35:03 -0700

    Add balanced bst

--- projects/bst_project/lib/leet_code_110.js
+++ projects/bst_project/lib/leet_code_110.js
@@ -2,6 +2,13 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
-
+var height = function(root){
+    if (!root) return -1;
+    return 1 + Math.max(height(root.left), height(root.right));
 }
+
+var isBalanced = function(root) {
+    if (!root) return true;
+    const balanced = Math.abs(height(root.left) - height(root.right)) <= 1;
+    return balanced && isBalanced(root.left) && isBalanced(root.right);
+};
