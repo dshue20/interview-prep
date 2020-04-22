ef684ce8dba71c9c64a7826eb9dbfadc0c663309
Derek Shue <dshue@berkeley.edu>
2020-04-22 16:44:09 -0700

    Add climb stairs problem

--- projects/tabulation_project/lib/leet_code_70.js
+++ projects/tabulation_project/lib/leet_code_70.js
@@ -1,6 +1,13 @@
 // Work through this problem on https://leetcode.com/problems/climbing-stairs/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function climbStairs(n) {
-
-}
+var climbStairs = function(n, memo={}) {
+    if (memo[n]) return memo[n];
+    if (!n) return 1;
+    if (n < 0) return 0;
+    let firstStep = climbStairs(n-1, memo);
+    let secondStep = climbStairs(n-2, memo);
+    memo[n-1] = firstStep;
+    memo[n-2] = secondStep;
+    return firstStep + secondStep;
+};
