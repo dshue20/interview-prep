ca5bc0bd7d43d572b375245a26e2c37aea0778f4
Derek Shue <dshue@berkeley.edu>
2020-04-28 15:15:04 -0700

    Add max swap

--- ruby/max_swap.rb
+++ ruby/max_swap.rb
@@ -15,9 +15,34 @@
 def swap(num, dig1, dig2)
     str = num.to_s
     str[dig1], str[dig2] = str[dig2], str[dig1]
-    str.to_i
+    str
+end
+
+def max_idx(num)
+    idx = 0
+    max = 0
+    (0...(num.to_s.length)).each do |digit|
+        test = num.to_s[digit].to_i
+        if test >= max
+            max = test
+            idx = digit
+        end
+    end
+    idx
 end
 
 def maximum_swap(num)
-    
+    return num if num < 10
+    str = num.to_s
+    max = max_idx(num)
+    length = num.to_s.length
+    return swap(num, 0, max).to_i if str[0] != str[max]
+    return (num.to_s[0] + maximum_swap(num.to_s[1..-1].to_i).to_s).to_i
 end
+
+p maximum_swap(2736)
+p maximum_swap(9973)
+p maximum_swap(1993)
+p maximum_swap(98368)
+
+# not quite
