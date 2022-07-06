05e3409da547636ee2cb2e496a48973a24b6f29b
Derek Shue <dshue@berkeley.edu>
2022-07-06 13:47:02 -0700

    Redo phone combos problem

--- javascript/phoneCombos.js
+++ javascript/phoneCombos.js
@@ -1,7 +1,7 @@
-// Given a string containing digits from 2-9 inclusive, return all possible letter 
+// Given a string containing digits from 2-9 inclusive, return all possible letter
 // combinations that the number could represent.
 
-// A mapping of digit to letters (just like on the telephone buttons) is given below. 
+// A mapping of digit to letters (just like on the telephone buttons) is given below.
 // Note that 1 does not map to any letters.
 
 // Example:
@@ -9,30 +9,30 @@
 // Input: "23"
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 
-var letterCombinations = function(digits) {
-    if (!digits.length) return [];
-    let combos = [""];
-    const mapping = {
-        2: ["a", "b", "c"],
-        3: ["d", "e", "f"],
-        4: ["g", "h", "i"],
-        5: ["j", "k", "l"],
-        6: ["m", "n", "o"],
-        7: ["p", "q", "r", "s"],
-        8: ["t", "u", "v"],
-        9: ["w", "x", "y", "z"]
-    };
-    for (let i=0; i < digits.length; i++){
-        let toAdd = mapping[digits[i]];
-        let updated = [];
-        toAdd.forEach(ele => {
-            combos.forEach(combo => {
-                updated.push(combo + ele);
-            })
-        })
-        combos = updated;
+var letterCombinations = function (digits) {
+  if (!digits.length) return [];
+  let mapping = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+  };
+
+  let strs = mapping[digits[0]];
+  for (let i = 1; i < digits.length; i++) {
+    let newStrs = [];
+    for (let str of strs) {
+      for (let letter of mapping[digits[i]]) {
+        newStrs.push(str + letter);
+      }
     }
-    return combos;
+    strs = newStrs;
+  }
+  return strs;
 };
 
-console.log(letterCombinations("23"));
+console.log(letterCombinations('23'));
